da5f2de645f62e0808f463a637afc695ad25ff49
khushi kandoi <khushikandoii09@gmail.com>
2024-04-04 18:03:19 +0530

    completed triangular image grid in signup

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,88 +1,125 @@
-import login1 from "../assets/images/login1.jfif";
-import p3 from "../assets/images/p3.jpg";
+import s1 from "../assets/images/s1.jpg";
+import s2 from "../assets/images/s2.jfif";
+import s3 from "../assets/images/s3.jfif";
+import s4 from "../assets/images/s4.jfif";
+import s5 from "../assets/images/s5.jfif";
+import s6 from "../assets/images/s6.jfif";
+import s7 from "../assets/images/s7.jfif";
+import s8 from "../assets/images/s8.jfif";
+import s9 from "../assets/images/s9.jfif";
+import s10 from "../assets/images/s10.jfif";
+import s11 from "../assets/images/s11.jfif";
+import s12 from "../assets/images/s12.jfif";
 const SignUp = () => {
   return (
     <>
       <style>
         {`
-      .rotate-signup-grid{
-        transform: skewX(35deg);
-        transform-origin: top left;
-      }
-      .diagonal-container {
-        position: relative;
-        overflow: hidden; /* Ensure content within pseudo-elements is not visible outside */
-      }
-      .diagonal-container::before,
-      .diagonal-container::after {
-        content: '';
-        position: absolute;
-        width: 100%;
-        height: 100%;
-        background-size: cover;
-      }
-      .diagonal-container::before {
-        clip-path: polygon(50% 56%, 100% 6%, 100% 100%, 6% 100%); /* Diagonal clip path for the first triangle */
-      }
-      .diagonal-container::after {
-        clip-path: polygon(0% 0%, 0% 0%, 92% 0%, 0% 92%); /* Diagonal clip path for the second triangle */
-      }
-      .div1::before{
-        background-image: url(${p3}); /* Replace with your image URL */
-      }
-      .div1::after{
-        background-image: url(${login1}); /* Replace with your image URL */
-      }
-      .div2::before{
-        background-image: url(${p3}); /* Replace with your image URL */
-      }
-      .div2::after{
-        background-image: url(${login1}); /* Replace with your image URL */
-      }
-      .div3::before{
-        background-image: url(${p3}); /* Replace with your image URL */
-      }
-      .div3::after{
-        background-image: url(${login1}); /* Replace with your image URL */
-      }
-      .div4::before{
-        background-image: url(${p3}); /* Replace with your image URL */
-      }
-      .div4::after{
-        background-image: url(${login1}); /* Replace with your image URL */
-      }
-      .div5::after{
-        background-image: url(${login1}); /* Replace with your image URL */
-      }
-      .div6::before{
-        background-image: url(${p3}); /* Replace with your image URL */
-      }
-      .div6::after{
-        background-image: url(${login1}); /* Replace with your image URL */
-      }
-      .div7::after{
-        background-image: url(${login1}); /* Replace with your image URL */
-      } 
-      
+        .triangular-img-card,.reverse-triangular-img-card{
+          margin:0 -55px;
+        }
+        .triangular-img-card{
+          padding-top:15px 
+        }
+        .reverse-triangular-img-card{
+          padding-bottom:10px 
+        }
+          .triangular-img{
+            clip-path: polygon(50% 0%, 0% 100%, 100% 100%);
+            overflow:hidden;
+          }
+          .reverse-triangular-img{
+            clip-path: polygon(50% 100%, 0 0, 100% 0);
+            overflow:hidden;
+          }
       `}
       </style>
       <div className="flex h-full w-full bg-black relative">
         <div className="flex w-full  top-0 left-0 justify-start absolute overflow-hidden">
-          <div className="flex w-[60%] gap-10 rotate-signup-grid">
-            <div className="flex flex-col h-screen w-full gap-8">
-              <div className="flex  w-full h-full diagonal-container div1"></div>
-              <div className="flex w-full h-full diagonal-container div2"></div>
-              <div className="flex w-full h-full diagonal-container div3"></div>
+          <div className="flex flex-col h-screen w-[70%] gap-[26px] overflow-hidden">
+            <div className="flex h-1/3 w-full px-[55px] overflow-hidden">
+              <div className="flex w-full reverse-triangular-img-card">
+                <img
+                  src={s12}
+                  className="reverse-triangular-img w-full h-full object-cover"
+                />
+              </div>
+              <div className="flex w-full triangular-img-card">
+                <img
+                  src={s2}
+                  className="triangular-img w-full h-full object-cover"
+                />
+              </div>
+              <div className="flex w-full reverse-triangular-img-card">
+                <img
+                  src={s10}
+                  className="reverse-triangular-img w-full h-full object-cover"
+                />
+              </div>
+              <div className="flex w-full triangular-img-card">
+                <img
+                  src={s9}
+                  className="triangular-img w-full h-full object-cover"
+                />
+              </div>
+              <div className="flex w-full reverse-triangular-img-card">
+                <img
+                  src={s5}
+                  className="reverse-triangular-img w-full h-full object-cover"
+                />
+              </div>
+              <div className="flex w-full triangular-img-card"></div>
             </div>
-            <div className="flex flex-col h-full w-full gap-8">
-              <div className="flex w-full h-full diagonal-container div4"></div>
-              <div className="flexw-full h-full diagonal-container div5"></div>
-              <div className="flex w-full h-full diagonal-container div6"></div>
+            <div className="flex h-1/3 w-full px-[55px] overflow-hidden">
+              <div className="flex w-full triangular-img-card"></div>
+              <div className="flex w-full reverse-triangular-img-card">
+                <img
+                  src={s6}
+                  className="reverse-triangular-img w-full h-full object-cover"
+                />
+              </div>
+              <div className="flex w-full triangular-img-card">
+                <img
+                  src={s7}
+                  className="triangular-img w-full h-full object-cover"
+                />
+              </div>
+              <div className="flex w-full reverse-triangular-img-card">
+                <img
+                  src={s8}
+                  className="reverse-triangular-img w-full h-full object-cover"
+                />
+              </div>
+              <div className="flex w-full triangular-img-card"></div>
+              <div className="flex w-full reverse-triangular-img-card"></div>
             </div>
-            <div className="flex flex-col  h-full w-full gap-8">
-              <div className="flex w-full h-full diagonal-container div7"></div>
-              <div className="flex  w-full h-full diagonal-container"></div>
-              <div className="flex w-full h-full diagonal-container"></div>
+            <div className="flex h-1/3 w-full px-[55px] overflow-hidden">
+              <div className="flex w-full reverse-triangular-img-card"></div>
+              <div className="flex w-full triangular-img-card"></div>
+              <div className="flex w-full reverse-triangular-img-card">
+                <img
+                  src={s3}
+                  className="reverse-triangular-img w-full h-full object-cover"
+                />
+              </div>
+              <div className="flex w-full triangular-img-card">
+                <img
+                  src={s1}
+                  className="triangular-img w-full h-full object-cover"
+                />
+              </div>
+              <div className="flex w-full reverse-triangular-img-card">
+                <img
+                  src={s11}
+                  className="reverse-triangular-img w-full h-full object-cover"
+                />
+              </div>
+              <div className="flex w-full triangular-img-card">
+                <img
+                  src={s4}
+                  className="triangular-img w-full h-full object-cover"
+                />
+              </div>
             </div>
           </div>
         </div>
