bbe9909dfef487403714328b797ebc56cd3d2cbc
khushi kandoi <khushikandoii09@gmail.com>
2024-08-27 15:50:55 +0530

    added header

--- src/components/Layout/Master.jsx
+++ src/components/Layout/Master.jsx
@@ -3,10 +3,10 @@ import Header from "./Header";
 const Master = ({ children }) => {
   return (
     <>
-      <div className="flex flex-col w-full h-full">
+      <div className="flex flex-col w-full h-full p-8 gap-8 bg-[var(--dark-gray)]">
         {/* Header section */}
         <div className="flex w-full sm:h-10">
-          <Header />
+          <Header user = {"john wick"}/>
         </div>
         {/* other child component */}
         <div className="w-full h-full">{children}</div>
